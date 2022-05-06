fbe0ffb37732ae567bf4c6d172c3b178fb1b08f9
unknown <narender@geekinformatic.com>
2022-05-06 12:13:59 +0530

    registraion functionality

--- src/app/shared/validations/passwordValidator.ts
+++ src/app/shared/validations/passwordValidator.ts
@@ -4,7 +4,7 @@ import { FormGroup } from '@angular/forms';
 export function MustMatch(controlName: string, matchingControlName: string) {
     return (formGroup: FormGroup) => {
         const control = formGroup.controls[controlName];
-        const matchingControl = formGroup.controls[matchingControlName];
+        const matchingControl = formGroup.controls[matchingControlName];       
 
         if (matchingControl.errors && !matchingControl.errors.mustMatch) {
             // return if another validator has already found an error on the matchingControl
