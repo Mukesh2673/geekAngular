d8b633e6fdb456ff7c410d1d0df9e36fa443b036
Mukesh2673 <mukeshbhatt12344@gmail.com>
2022-07-01 19:06:31 +0530

    complete crud

--- src/app/modules/admins/admins-list/admins-list.component.ts
+++ src/app/modules/admins/admins-list/admins-list.component.ts
@@ -6,94 +6,60 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-admins-list',
   templateUrl: './admins-list.component.html',
-  styleUrls: ['./admins-list.component.scss']
+  styleUrls: ['./admins-list.component.scss'],
 })
 export class AdminsListComponent implements OnInit {
-  test!:string
-  constructor(private apiService:ApiService,
-    private toastr:ToastrService,
-    private router: Router) { }
-
+  test!: string;
+  constructor(
+    private apiService: ApiService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
     this.adminData();
   }
-
-
-
-adminData(){
-  this.apiService.getData("admins/getAdmindata").subscribe(
-    (result: any) => {   
-     this.test = result       
-        console.log('okkk',result);
-     
-    },
-    (error)=>{
-    //this.hasError=true;
-    //this.toastr.error(error.error.responseMessage,"Error!");
-    console.log("error inside");
-    })
+  //get Admin Data
+  adminData() {
+    this.apiService.getData('admins/getAdmindata').subscribe(
+      (result: any) => {
+        this.test = result;
+      },
+      (error) => {
+        console.log('error inside');
+      }
+    );
   }
-  deleteAdmin(id:any){
+  //delete Admin Data
+  deleteAdmin(id: any) {
     this.apiService.deleteData(`admins/deleteAdmin/${id}`).subscribe(
-      (result: any) => {   
-        if(result.responseCode===200){
-          // Handle result
-          console.log('success')
-        } 
+      (result: any) => {
+        if (result?.responseCode === 200) {
+          console.log('success');
+        }
       },
-      (error)=>{
-      //this.hasError=true;
-      //this.toastr.error(error.error.responseMessage,"Error!");
-      console.log("error inside",error);
+      (error) => {
+        console.log('error inside', error);
       },
-       ()=>{
-        this.toastr.success("Successfully Deleted.", "Success!");
+      () => {
+        this.toastr.success('Successfully Deleted.', 'Success!');
         this.adminData();
-      } 
-
-    )
+      }
+    );
   }
-  updateAdmin(id:any){
-    console.log('id of element is',id)
+  //editAdmin data
+  editAdmin(id: any) {
     this.apiService.getData(`admins/getAdminData/${id}`).subscribe(
       (result: any) => {
-        console.log('data to updata',result)   
-        if(result.responseCode===200){
-          // Handle result
-          console.log('success')
-        } 
+        if (result?.responseCode === 200) {
+          console.log('success');
+        }
       },
-      (error)=>{
-      //this.hasError=true;
-      //this.toastr.error(error.error.responseMessage,"Error!");
-      console.log("error inside",error);
+      (error) => {
+        console.log('error inside', error);
       },
-       ()=>{
-          //this.toastr.success("Successfully updated", "Success!");
-        //this.adminData();
+      () => {
         this.router.navigateByUrl(`/admins/edit/${id}`);
-      } 
-
-    )
-
-
-
-
-
-
-
-
-
-
-
-
+      }
+    );
   }
-
-
-
-
-
-
-
-
 }
