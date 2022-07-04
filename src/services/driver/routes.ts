31e3e88bc345a39602ab8af3e5aeceaec22e5393
GT81291 <mukeshbhatt12344@gmail.com>
2022-07-04 19:27:44 +0530

    create routes for drivers

---
+++ src/services/driver/routes.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from "express";
+import {getDriverData,deleteDriver,getDriverDataToUpdate,patchDriver} from "./controller";
+import config from "config";
+import { checkAuthenticate } from "../../middleware/checks";
+const basePath = config.get("BASE_PATH");
+const currentPath = "drivers";
+const currentPathURL = basePath + currentPath;
+export default [
+ /*  {
+    path: currentPathURL + "/addAdmin",
+    method: "post",
+    handler: [
+      async (req: Request, res: Response) => {
+
+        const result = await addAdmin(req.body);
+        res.status(200).send(result);
+      }
+    ]
+  }, */
+  {
+    path: currentPathURL + "/getDriversdata",
+    method: "get",
+    handler: [
+      async (req: Request, res: Response) => {
+          const result = await getDriverData();
+          res.status(200).send(result);
+      }
+    ]
+  },
+  {
+    path: currentPathURL + "/getDriverdata/:id",
+    method: "get",
+    handler: [
+      async (req: Request, res: Response) => {
+          //console.log('get Admins data from mongoss');
+          const result = await getDriverDataToUpdate(req.params.id);
+          res.status(200).send(result);
+      }
+    ]
+  },
+  {
+    path: currentPathURL + '/deleteDriver/:id',
+    method: "delete",
+    handler: [
+      async (req: Request, res: Response) => {
+          console.log('get  data to delete',req.params);
+          const result = await deleteDriver(req.params.id);
+          res.status(200).send(result);
+      }
+    ]
+  },
+  {
+    path: currentPathURL + '/patchDriver',
+    method: "put",
+    handler: [
+      async (req: Request, res: Response) => {
+          const result = await patchDriver(req.body);
+          res.status(200).send(result);
+      }
+    ]
+  },
+  
+];
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,10 +2,12 @@ import authRoutes from "./auth/routes";
 import healthCheckRoutes from "./healthCheck/routes";
 import userRoutes from "./user/routes";
 import adminRoutes from './admin/routes';
+import driverRoutes from './driver/routes'
 
 export default [
     ...authRoutes, 
     ...healthCheckRoutes,
     ...userRoutes,
-    ...adminRoutes
+    ...adminRoutes,
+    ...driverRoutes
 ];
