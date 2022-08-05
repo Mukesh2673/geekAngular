f63c80c15299ae76566e224fa83f6c3db13472ea
mukesh <development.geektech@gmail.com>
2022-08-05 18:51:23 +0530

    make reactive form in edit profile section and add imageupload feature in progress

--- src/app/components/pages/profile/profile01/profile01.component.ts
+++ src/app/components/pages/profile/profile01/profile01.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-profile01',
   templateUrl: './profile01.component.html',
   styleUrls: ['./profile01.component.scss']
 })
-export class Profile01Component implements OnInit {
 
-  constructor() { }
+
+
+
+
+export class Profile01Component implements OnInit {
+public userDetails:any
+  constructor(private apiService: ApiService) {
+     this.userDetails=apiService.getUserDetails()
+  
+    }
 
   ngOnInit(): void {
+  console.log(this.userDetails)
   }
 
 }
